479fb53daee444438987d8ab90bd7aea8445a11e
Eric Williams <eric@churchwilliams.com>
2015-02-27 13:46:55 -0700

    pretty sure this is finalized. Need to review it though

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,11 +1,12 @@
-var app = angular.module('directiveWorkshop', []);
+var app = angular.module('directiveWorkshop', ['myDirectives']);
 
 app.controller('mainCtrl', function($scope, mainService){
 $scope.getData = function () {
-        return mainService.getData($scope.query).then(function (data) {
-           console.log(data);
-           return $scope.data = data;
-        });
+        return mainService.getData($scope.query)
+        	.then(function (data) {
+           		console.log(data);
+           		return $scope.data = data;
+        	});
     };
-
+    $scope.title = 'Test';
 });
